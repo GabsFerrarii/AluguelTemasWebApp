9997b3515c14d846ea6b9d96b648cd7ce9d96743
Gabriel Ferreira Cavalcante <cavalcante.gabriel@academico.ifrn.edu.br>
2023-11-10 18:00:59 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,48 @@
+﻿using Modelo;
+using Servico;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AluguelTemasWebApp.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ItemServico itemServico = new ItemServico();
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+        private ActionResult GravarItem(ItemTema item)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    itemServico.GravarItem(item);
+                    return RedirectToAction("Index");
+                }
+                return View(item);
+            }
+            catch
+            {
+                return View(item);
+            }
+        }
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ItemTema item)
+        {
+            return GravarItem(item);
+        }
+    }
+}
